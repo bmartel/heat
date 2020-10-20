62b76df37daeb984dd9226a272958c1e8f430549
Brandon Martel <brandonmartel@gmail.com>
2020-10-19 19:22:17 -0500

    user example throws errors since props are defined with decorators and no initialization

--- packages/tql-template/packages/api/src/entity/User.ts
+++ packages/tql-template/packages/api/src/entity/User.ts
@@ -3,14 +3,8 @@ import { Entity, ObjectIdColumn, ObjectID, Column } from 'typeorm';
 @Entity()
 export class User {
   @ObjectIdColumn()
-  id: ObjectID;
+  id!: ObjectID;
 
   @Column()
-  firstName: string;
-
-  @Column()
-  lastName: string;
-
-  @Column()
-  age: number;
+  email!: string;
 }
