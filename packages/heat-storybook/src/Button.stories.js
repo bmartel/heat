d440bd3c809aa049b3b20eda1d6c084f3f3f4d42
Brandon Martel <brandonmartel@gmail.com>
2020-10-29 19:37:53 -0500

    make children props autowrapped with hyperapp text

--- packages/heat-storybook/src/Button.stories.js
+++ packages/heat-storybook/src/Button.stories.js
@@ -1,40 +1,40 @@
-import { Button } from './Button';
-import { Component, t } from './hyperapp';
+import { Button } from "./Button";
+import { Component } from "./hyperapp";
 
 const Template = (args) =>
-  Component({ view: Button, children: t('Button'), ...args });
+  Component({ view: Button, children: "Button", ...args });
 
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Template,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 };
 
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
-  children: t('Primary Button'),
+  label: "Button",
+  children: "Primary Button",
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
-  children: t('Secondary Button'),
+  label: "Button",
+  children: "Secondary Button",
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
-  children: t('Large Button'),
+  size: "large",
+  label: "Button",
+  children: "Large Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
-  children: t('Small Button'),
+  size: "small",
+  label: "Button",
+  children: "Small Button",
 };
--- packages/heat-storybook/src/hyperapp.js
+++ packages/heat-storybook/src/hyperapp.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { app, text } from 'hyperapp';
+import React from "react";
+import { app, text } from "hyperapp";
 
 export const t = text;
 
 const el = React.createElement;
+
+const renderChildren = (children) => {
+  return Array.isArray(children)
+    ? children.map(renderChildren)
+    : typeof children === "string" || typeof children === "number"
+    ? text(children)
+    : children;
+};
 
 export const Component = ({ children, className, view, ...props }) => {
   const dom = React.useRef();
@@ -11,10 +19,10 @@ export const Component = ({ children, className, view, ...props }) => {
   React.useEffect(() => {
     app({
       init: { ...props },
-      view: (state) => view(state, children),
+      view: (state) => view(state, renderChildren(children)),
       node: dom.current,
     });
   }, [props, children, view]);
 
-  return el('div', null, el('div', { ref: dom }));
+  return el("div", null, el("div", { ref: dom }));
 };
